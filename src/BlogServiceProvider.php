7774584074dec5f752454d2962e2f564fd261358
Ömer Can <senin@email.adresin>
2025-12-31 22:40:56 +0300

    Fix route paths and ensure web routes exist

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -14,7 +14,11 @@ class BlogServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadRoutesFrom(__DIR__.'/../routes/admin.php');
+
+        if (file_exists(__DIR__.'/../routes/admin.php')) {
+            $this->loadRoutesFrom(__DIR__.'/../routes/admin.php');
+        }
+
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Omercanfs\BlogCore\Models\Post;
 
 Route::get('/blog-test', function () {
-    return Post::all();
+    return [];
 });
