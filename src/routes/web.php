0d2aa24a768937496067e9e6d4d0d7ed1682d2fa
Ömer Can <senin@email.adresin>
2025-12-31 23:42:27 +0300

    Fix package directory structure

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,10 +12,22 @@ Route::prefix('admin/blog')->group(function () {
     Route::get('/posts', [PostController::class, 'index'])
         ->name('admin.blog.posts.index');
 
+    Route::get('/posts/create', [PostController::class, 'create'])
+        ->name('admin.blog.posts.create');
+
     Route::post('/posts', [PostController::class, 'store'])
         ->name('admin.blog.posts.store');
 
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])
+        ->name('admin.blog.posts.edit');
+
+    Route::put('/posts/{post}', [PostController::class, 'update'])
+        ->name('admin.blog.posts.update');
+
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])
+        ->name('admin.blog.posts.destroy');
 });
+
 
 
 
