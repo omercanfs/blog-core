d21236a47dfa0abd1ad11ac5af2c0deff969d891
Ömer Can <senin@email.adresin>
2025-12-31 23:52:39 +0300

    Initial blog-core package structure

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -4,14 +4,24 @@ use Illuminate\Support\Facades\Route;
 use Omercanfs\BlogCore\Http\Controllers\Admin\PostController;
 
 Route::prefix('admin/blog')
-    ->middleware(['web', 'auth'])
+    ->name('admin.blog.')
     ->group(function () {
 
-        Route::get('/posts', [PostController::class, 'index']);
-        Route::get('/posts/create', [PostController::class, 'create']);
-        Route::post('/posts', [PostController::class, 'store']);
-        Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
-        Route::put('/posts/{post}', [PostController::class, 'update']);
-        Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+        Route::get('/posts', [PostController::class, 'index'])
+            ->name('posts.index');
 
+        Route::get('/posts/create', [PostController::class, 'create'])
+            ->name('posts.create');
+
+        Route::post('/posts', [PostController::class, 'store'])
+            ->name('posts.store');
+
+        Route::get('/posts/{post}/edit', [PostController::class, 'edit'])
+            ->name('posts.edit');
+
+        Route::put('/posts/{post}', [PostController::class, 'update'])
+            ->name('posts.update');
+
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])
+            ->name('posts.destroy');
     });
