5a746ce9e9bf8357e43c897d00c2f4513a2019be
Ömer Can <senin@email.adresin>
2025-12-31 21:50:54 +0300

    Initial blog-core package structure

---
+++ src/BlogServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Omercanfs\BlogCore;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
