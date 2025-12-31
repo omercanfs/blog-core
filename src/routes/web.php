4ae7d7e647c3b8a38f16d95e4a5b0aace07d36d3
Ömer Can <senin@email.adresin>
2025-12-31 23:26:44 +0300

    Auto generate unique slug for blog posts

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Omercanfs\BlogCore\Http\Controllers\Admin\PostController;
 
 Route::get('/blog-test', function () {
     return [];
 });
+
+Route::prefix('admin/blog')->group(function () {
+
+    Route::get('/posts', [PostController::class, 'index'])
+        ->name('admin.blog.posts.index');
+
+    Route::post('/posts', [PostController::class, 'store'])
+        ->name('admin.blog.posts.store');
+
+});
+
+
+
